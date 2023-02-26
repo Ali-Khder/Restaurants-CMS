d0bfaa2ba8117cf9d97f1d579d00d915a0089498
unknown <alikhder2012@example.com>
2023-02-26 11:21:18 +0300

    SSR/ larave-vue mix

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="icon" href="/favicon.ico">
+    <title>media-app-vue</title>
+    <script defer="defer" src="/js/chunk-vendors.a9ffde3c.js"></script>
+    <script defer="defer" src="{{ mix('/public/js/app.js') }}"></script>
+    <link href="/css/chunk-vendors.269fb860.css" rel="stylesheet">
+    <link href="{{ mix('/public/css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<noscript><strong>We're sorry but media-app-vue doesn't work properly without JavaScript enabled. Please enable it to
+        continue.</strong></noscript>
+<div id="app"></div>
+</body>
+</html>
